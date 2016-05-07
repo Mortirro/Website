bf85a43abf3b4edc63662c6a5771f44a48441c26
Mortirro <denmortirro@gmail.com>
2016-05-07 17:54:39 +0300

    add fancy box

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
-  $(".diploms__item-fancybox").fancybox({
-    openEffect	: 'none',
-    closeEffect	: 'none'
+  
+  // Скрыть / показать меню
+  $(".header").mouseenter(function() {
+    $(this).animate({ opacity: "1" }, 400);
   });
+
+  $(".header").mouseleave(function() {
+    $(this).stop().animate({ opacity: "0" }, 400);
+  });
+//Услуги
+$(".item-fancybox").fancybox({
+  openEffect : 'none',
+  closeEffect : 'none'
+ });
+
+  $(".various").fancybox({
+  maxWidth : 1200,
+  maxHeight : 800,
+  fitToView : false,
+  width  : '70%',
+  height  : '70%',
+  autoSize : false,
+  closeClick : false,
+  openEffect : 'none',
+  closeEffect : 'none'
+ });
 });
