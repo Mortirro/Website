d2470be6d29eb7dbe0b78715fbe63d454f0028c8
Mortirro <denmortirro@gmail.com>
2016-05-11 22:17:11 +0300

    added photos to the class gallery

--- js/app.js
+++ js/app.js
@@ -1,28 +1,23 @@
 $(document).ready(function() {
-  
-  // Скрыть / показать меню
-  $(".header").mouseenter(function() {
-    $(this).animate({ opacity: "1" }, 400);
+  $(".diploms__item-fancybox").fancybox({
+    openEffect	: 'none',
+    closeEffect	: 'none'
   });
 
-  $(".header").mouseleave(function() {
-    $(this).stop().animate({ opacity: "0" }, 400);
-  });
-//Услуги
-$(".item-fancybox").fancybox({
-  openEffect : 'none',
-  closeEffect : 'none'
- });
+  $(".item-fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
 
   $(".various").fancybox({
-  maxWidth : 1200,
-  maxHeight : 800,
-  fitToView : false,
-  width  : '70%',
-  height  : '70%',
-  autoSize : false,
-  closeClick : false,
-  openEffect : 'none',
-  closeEffect : 'none'
- });
+		maxWidth	: 1000,
+		maxHeight	: 800,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
 });
